80ffc1e98fe883c8ba27f416d875b59445051958
Luis Palacios <lrpalaciosdev@gmail.com>
2019-10-10 14:01:44 -0600

    initial migration

---
+++ DataAccess/RestaurantContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCommonPitFalls.DataAccess
+{
+    public class RestaurantContext : DbContext
+    {
+        public DbSet<MenuCategory> MenuCategories { get; set; }
+
+        public RestaurantContext()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=(localdb)\\mssqllocaldb;Database=RestaurantDb;Trusted_Connection=True;MultipleActiveResultSets=true"
+                );
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
